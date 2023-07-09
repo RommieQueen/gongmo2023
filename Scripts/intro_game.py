1f9fb1383ad97504c93718c48fae5ae31f050bc6
Junseo Won <junseowon@gmail.com>
2023-07-09 20:47:29 +0900

    add system

--- Scripts/intro_game.py
+++ Scripts/intro_game.py
@@ -1,3 +1,179 @@
 import pygame
 
-pygame 
+pygame.init()
+
+def intro():
+    
+    from main import screen, BLACK, WIDTH_CENTER, HEIGHT_CENTER, FPS
+
+    alpha = 0
+    story = 0
+
+    background_pos = (0, 0)
+            
+    dialog = pygame.image.load('./../Images/dialog.png')
+        
+    intro_img_1 = pygame.image.load('./../Images/intro_park.png')
+    intro_img_2 = pygame.image.load('./../Images/intro_park2.png')
+        
+    intro_font = pygame.font.Font('./../Fonts/NeoDunggeunmoPro-Regular.ttf', 50)
+    sys_font = pygame.font.Font('./../Fonts/NeoDunggeunmoPro-Regular.ttf', 15)
+
+    system_text = sys_font.render("▼SPACE를 눌러 진행▼", True, BLACK)
+    system_text_pos = system_text.get_rect(center=(WIDTH_CENTER, HEIGHT_CENTER + 320))
+    
+    hero_name = intro_font.render("나", True, BLACK)
+    hero_name_pos = hero_name.get_rect(center = (WIDTH_CENTER - 440, HEIGHT_CENTER + 20))
+    
+    hero_text_1 = intro_font.render("오늘은 오랜만에 남자친구를 만나기 위해서 공원을 왔다", True, BLACK)
+        
+    hero_text_2 = intro_font.render("이 공원... 굉장히 오랜만에 오는 것 같다...", True, BLACK)
+    hero_text_3 = intro_font.render("예전에는 많이 와서 산책도 했는데 요즘은 쓰레기를", True, BLACK)
+    hero_text_4 = intro_font.render("버리는 사람들이 늘면서 호수가 많이 더러워 진 것 같다", True, BLACK)
+
+    hero_text_5 = intro_font.render("30분이 지났는데 안 오고 있다... 무슨 일이 있는걸까?", True, BLACK)
+
+    hero_text_6 = intro_font.render("얼마나 기다렸을까... 갑자기 짜증이 몰려오기 시작했다", True, BLACK)
+    hero_text_7 = intro_font.render("얼마나 더 기다려야 할까...", True, BLACK)
+    hero_text_8 = intro_font.render("방금 무슨 소리지?!", True, BLACK)
+    
+
+    alien_image = pygame.image.load('./../Images/alien.png')
+    alien_image_pos = alien_image.get_rect(center=(WIDTH_CENTER + 450, HEIGHT_CENTER - 85))
+    
+    alien_name = intro_font.render("???", True, BLACK)
+    alien_name_pos = alien_name.get_rect(center = (WIDTH_CENTER - 440, HEIGHT_CENTER + 20))
+
+    alien_text_1 = intro_font.render("&$*%&$%*%$%&*#$@*#&#@*%&*%*$&*#$#*$&#*$&*#$&*", True, BLACK)
+    
+    
+    character_text_pos_top = (15, 420)
+    character_text_pos_middle = (15, 480)
+    character_text_pos_bottom = (15, 540)
+        
+    running = True
+
+    while running:
+
+        deltaTime = FPS.tick(60)
+            
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and alpha == 255:
+                    alpha = 0
+                    story += 1
+                    print(story)
+
+        screen.fill(BLACK)
+            
+        if story == 0:
+                
+            alpha += 1.5
+                
+            if alpha > 255:
+                alpha = 255
+                    
+            intro_img_1.set_alpha(alpha)
+            screen.blit(intro_img_1, background_pos)
+            
+            if alpha == 255:
+                screen.blit(dialog, background_pos)
+                screen.blit(hero_text_1, character_text_pos_top)
+                screen.blit(hero_name, hero_name_pos)
+                screen.blit(system_text, system_text_pos)
+
+        elif story == 1:
+            
+            alpha += 1.5
+                
+            if alpha > 255:
+                alpha = 255
+                    
+            intro_img_2.set_alpha(alpha)
+            screen.blit(intro_img_2, background_pos)
+            
+            if alpha == 255:
+                screen.blit(dialog, background_pos)
+                screen.blit(hero_text_2, character_text_pos_top)
+                screen.blit(hero_text_3, character_text_pos_middle)
+                screen.blit(hero_text_4, character_text_pos_bottom)
+                screen.blit(hero_name, hero_name_pos)
+                screen.blit(system_text, system_text_pos)
+                    
+        elif story == 2:
+            
+            alpha += 1.5
+                
+            if alpha > 255:
+                alpha = 255
+                    
+            intro_img_2.set_alpha(alpha)
+            screen.blit(intro_img_2, background_pos)
+            
+            if alpha == 255:
+                screen.blit(dialog, background_pos)
+                screen.blit(hero_text_5, character_text_pos_top)
+                screen.blit(hero_name, hero_name_pos)
+                screen.blit(system_text, system_text_pos)
+
+        elif story == 3:
+            
+            alpha += 1.5
+                
+            if alpha > 255:
+                alpha = 255
+                    
+            intro_img_2.set_alpha(alpha)
+            screen.blit(intro_img_2, background_pos)
+            
+            if alpha == 255:
+                screen.blit(dialog, background_pos)
+                screen.blit(hero_text_6, character_text_pos_top)
+                screen.blit(hero_text_7, character_text_pos_middle)
+                screen.blit(hero_name, hero_name_pos)
+                screen.blit(system_text, system_text_pos)
+
+        elif story == 4:
+            
+            alpha += 1.5
+                
+            if alpha > 255:
+                alpha = 255
+                    
+            intro_img_2.set_alpha(alpha)
+            screen.blit(intro_img_2, background_pos)
+            
+            if alpha == 255:
+                screen.blit(dialog, background_pos)
+                screen.blit(alien_text_1, character_text_pos_top)
+                screen.blit(alien_name, alien_name_pos)
+                screen.blit(system_text, system_text_pos)
+                
+        elif story == 5:
+            alpha = 255
+            screen.blit(intro_img_2, background_pos)
+            
+            screen.blit(dialog, background_pos)
+            screen.blit(hero_text_8, character_text_pos_top)
+            screen.blit(hero_name, hero_name_pos)
+            screen.blit(system_text, system_text_pos)
+
+        elif story == 6:
+            alpha = 255
+            screen.blit(intro_img_2, background_pos)
+
+            screen.blit(alien_image, alien_image_pos)
+            screen.blit(dialog, background_pos)
+            screen.blit(alien_text_1, character_text_pos_top)
+            screen.blit(alien_name, alien_name_pos)
+            screen.blit(system_text, system_text_pos)
+                
+        
+                
+        pygame.display.update()
+                    
+    pygame.quit()
+
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,12 +1,15 @@
 import pygame
-from game_manager import description
+from game_manager import button
+from intro_game import intro
 
 pygame.init()
+
+FPS = pygame.time.Clock()
 
 WIDTH_SCREEN = 1280
 HEIGHT_SCREEN = 720
 screen = pygame.display.set_mode((WIDTH_SCREEN, HEIGHT_SCREEN))
-pygame.display.set_caption("pygame")
+pygame.display.set_caption("본격! 외계인 침공은 있던적이 없던거야!")
 
 WIDTH_CENTER = WIDTH_SCREEN // 2
 HEIGHT_CENTER = HEIGHT_SCREEN // 2
@@ -18,88 +21,33 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-def menu():
-    #변수선언
-    background = pygame.image.load('Images/start_background.png')
+def main_window():
 
-    title_font = pygame.font.Font('Fonts/NeoDunggeunmoPro-Regular.ttf', 60)
+    background = pygame.image.load('./../Images/start_background.png')
+
+    title_font = pygame.font.Font('./../Fonts/NeoDunggeunmoPro-Regular.ttf', 60)
     title_text = title_font.render("본격! 외계인 침공은 있던적이 없던거야!", True, BLACK)
     title_text_rect = title_text.get_rect(center=(WIDTH_CENTER, HEIGHT_CENTER - 200))
 
-    button_font = pygame.font.Font('Fonts/NeoDunggeunmoPro-Regular.ttf', 40)
-
-    start_text = button_font.render("시 작",True,WHITE)
-    start_text_rect = start_text.get_rect(center = (WIDTH_CENTER, HEIGHT_CENTER))
-
-    descrip_text = button_font.render("설 명",True,WHITE)
-    descrip_text_rect = descrip_text.get_rect(center = (WIDTH_CENTER, (HEIGHT_CENTER + 110)))
-
-    start_button = pygame.Rect((WIDTH_CENTER - 75), (HEIGHT_CENTER - 40), 150, 80)
-    descrip_button = pygame.Rect((WIDTH_CENTER - 75), (HEIGHT_CENTER + 70), 150, 80)
+    start_button = pygame.image.load('./../Images/start_button.png')
     
-    #실행
-    run = True
-    while run :                    
+    running = True
+
+    while running:
+        
+        deltaTime = FPS.tick(60)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
         screen.blit(background,(0, 0))
         screen.blit(title_text, title_text_rect)
-        
-        pygame.draw.rect(screen, BLUE, start_button, border_radius = 5)
-        screen.blit(start_text, start_text_rect)
-        
-        pygame.draw.rect(screen, BLUE, descrip_button, border_radius = 5)
-        screen.blit(descrip_text, descrip_text_rect)
+        button(screen, WIDTH_CENTER - 128, HEIGHT_CENTER - 67, start_button, 256, 134, intro)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if start_button.collidepoint(mouse_pos):
-                    intro()
-                elif descrip_button.collidepoint(mouse_pos):
-                    run = description(run, screen, WHITE)                
-
-        if run==True:
+        if running == True:
             pygame.display.update()
-            
+        
     pygame.quit()
 
-def intro():
-    
-    alpha = 0
-    timer = 0
-    intro_img1 = pygame.image.load('Images/intro_park.png')
-    intro_img2 = pygame.image.load('Images/intro_park2.png')
-    intro_font = pygame.font.Font('Fonts/NeoDunggeunmoPro-Regular.ttf', 50)
-    intro_text1 = intro_font.render("2023년 7월 3일", True, WHITE)
-    intro_text2 = intro_font.render("공원에 오신것을 환영합니다.",True,WHITE)
-    intro_text1_rect = intro_text1.get_rect(center=(WIDTH_CENTER, HEIGHT_CENTER-250))
-    intro_text2_rect = intro_text2.get_rect(center=(WIDTH_CENTER, HEIGHT_CENTER-150))
-
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                
-        screen.fill(BLACK)
-        
-        #불투명도 조절
-        alpha+=1.5
-        if alpha > 255:
-            alpha = 255
-            
-        intro_img1.set_alpha(alpha)
-        screen.blit(intro_img1, (0,0))
-        
-        if alpha == 255:
-            screen.blit(intro_text1, intro_text1_rect)
-            screen.blit(intro_text2, intro_text2_rect)
-        timer+=1
-        if timer >= 450:
-            timer = 450
-            screen.blit(intro_img2,(0,0))
-        pygame.display.update()
-    pygame.quit()
-menu()
+main_window()
