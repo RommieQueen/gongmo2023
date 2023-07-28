6485e552ba7ae2dfc1ad05cd0bd0fc53b0000749
Rommie <miniopl802222@gmail.com>
2023-07-28 17:39:06 +0900

    tq

--- Scripts/ground.py
+++ Scripts/ground.py
@@ -4,7 +4,7 @@ import time
 
 screen_width, screen_height = 1280, 720
 
-SKYBLUE = (178,235,244)
+SKY = (124,150,201)
 # 땅(Ground) 클래스 정의
 class Ground(pygame.sprite.Sprite):
     def __init__(self, ground_image_path):
@@ -13,20 +13,22 @@ class Ground(pygame.sprite.Sprite):
         self.image_width = self.image.get_width()
         self.x_pos = 0
 
+        #draw
+        self.is_ground = True
+
     def update(self, player_is_move, player_rect_x, player_speed):            
         if player_is_move == True:
-            # player 위치가 화면 끝 근처인 경우, 원래 속도를 player 속도로 변경
+            # player 위치가 화면 끝 근처인 경우, 원래 속도(0)를 player 속도로 변경
             if player_rect_x <= 160:
                 self.x_pos += player_speed
             elif player_rect_x >= 1220:
                 self.x_pos -= player_speed
 
     def draw(self, screen, plus_x):
-        screen.fill(SKYBLUE)
-        for i in range(1, 10):
-            screen.blit(self.image, (self.x_pos, 620))
-            self.x_pos += plus_x
+        screen.fill(SKY)
 
+        for x in range (0, 1281, 128): #for문의 x를 x좌표로 그냥 사용해버림 0~1280 128씩 증가 (이미지가 128*128)
+            screen.blit(self.image, (x, 620))
         if self.x_pos >= 0:
             screen.blit(self.image, (self.x_pos - self.image_width, 620))
         elif self.x_pos <= 1280:
